f5fbfb29809b0c82184f1e35f6d5317fa5e6beb5
Alejandro Godinez <programmergodinez@gmail.com>
2025-06-11 23:08:10 -0500

    updated notepad++ macros added microsoft sql msm macros rename tones to last entry

--- macros/12_notepad++.py
+++ macros/12_notepad++.py
@@ -11,21 +11,21 @@ app = {                      # REQUIRED dict, must be named 'app'
     'macros' : [             # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x000040, 'Compare',   [Keycode.CONTROL, Keycode.ALT, 'c']),
-        (0x000000, '',          []),
-        (0x000040, 'Align',     [Keycode.CONTROL, Keycode.SHIFT, '=']),
+        (0x400040, 'MoveTab', [Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, 'v']),
+        (0x400040, 'Fld',     [Keycode.ALT, '0']),
+        (0x400040, 'UFld',    [Keycode.ALT, Keycode.SHIFT, '0']),
         # 2nd row ----------
-        (0x004000, 'UCase',          [Keycode.CONTROL, Keycode.SHIFT, 'u']),
-        (0x000000, '',          []),
-        (0x004000, 'LCase',          [Keycode.CONTROL, 'u']),
+        (0x00F000, 'UCase',          [Keycode.CONTROL, Keycode.SHIFT, 'u']),
+        (0x00F000, 'LCase',          [Keycode.CONTROL, 'u']),
+        (0xF0F0F0, 'SpcTab',       [Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, Keycode.TAB]),
         # 3rd row ----------
-        (0x000000, '',          []),
-        (0x000000, '',          []),
-        (0x000000, '',          []),
+        (0x0000F0, 'Compare',   [Keycode.CONTROL, Keycode.ALT, 'c']),
+        (0xF06040, '[clr]',     [Keycode.CONTROL, Keycode.ALT, 'x']),
+        (0x0000F0, 'VSync',          [Keycode.CONTROL, Keycode.SHIFT, 'v']),
         # 4th row ----------
-        (0x000000, '',          []),
-        (0x000000, '',          []),
-        (0x000000, '',          []),
+        (0x8B5A00, 'Dir',          [Keycode.CONTROL, Keycode.SHIFT, 'e']),
+        (0x0000FF, 'Align',     [Keycode.CONTROL, Keycode.SHIFT, '=']),
+        (0x6000FF, 'JSFrmt',          [Keycode.CONTROL, Keycode.ALT, 'm']),
         # Encoder button ---
         (0x000000, '',          [True]) #boolean = use as light toggle
     ]
