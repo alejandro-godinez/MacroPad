6025f92c5831005daa6ba0b3d7f0c184c458ca35
Alejandro Godinez <programmergodinez@gmail.com>
2026-04-16 00:45:56 -0500

    remove color fro unused macro keys

--- macros/15_Delphi7.py
+++ macros/15_Delphi7.py
@@ -6,16 +6,16 @@ app = {                      # REQUIRED dict, must be named 'app'
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x400040, 'Run',     [Keycode.F9]),
-        (0x400040, '',        []),
-        (0x400040, '',        []),
+        (0x000000, '',        []),
+        (0x000000, '',        []),
         # 2nd row ----------
         (0x000000, 'Grep',    [Keycode.SHIFT, Keycode.ALT, 'S']),
         (0x400000, 'Units',   [Keycode.CONTROL, Keycode.F12]),
         (0x000000, 'Forms',   [Keycode.SHIFT, Keycode.F12]),
         # 3rd row ----------
-        (0x004000, '',        []),
-        (0x004000, '',        []),
-        (0xF0F0F0, '',        []),
+        (0x000000, '',        []),
+        (0x000000, '',        []),
+        (0x000000, '',        []),
         # 4th row ----------
         (0x000000, '',        []),
         (0x000040, '',        []),
