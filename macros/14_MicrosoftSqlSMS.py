3ae5a63440b4dc21c2d1c5a900aaf410c3870fc5
Alejandro Godinez <programmergodinez@gmail.com>
2026-04-16 00:35:40 -0500

    12_MicrosoftSqlSMS.py - added duplicate tab macro

--- macros/14_MicrosoftSqlSMS.py
+++ macros/14_MicrosoftSqlSMS.py
@@ -17,9 +17,12 @@ app = {                      # REQUIRED dict, must be named 'app'
         (0x004000, 'LCase',      [Keycode.CONTROL, 'u']),
         (0xF0F0F0, 'WSpace',     [Keycode.CONTROL, 'r', Keycode.CONTROL, 'w']),
         # 4th row ----------
-        (0x000000, '',               []),
-        (0x000040, 'Refresh Cache',  [Keycode.CONTROL, Keycode.SHIFT, 'r']),
-        (0x000000, '',               []),
+        (0x000040, 'Re-Cache',   [Keycode.CONTROL, Keycode.SHIFT, 'r']),
+        (0x000000, '',           []),
+        (0x000000, 'Duplicate',  [Keycode.CONTROL,'a',   #select all
+                                  Keycode.CONTROL,'c',   #copy
+                                  Keycode.CONTROL,'n',   #new query
+                                  Keycode.CONTROL,'v']), #paste
         # Encoder button ---
         (0x000000, '',               [True]) #boolean = use as light toggle
     ]
